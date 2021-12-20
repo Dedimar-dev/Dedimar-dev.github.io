13b2b60dee1d61804e0210b8be346912d86cf2bb
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-12-20 13:26:54 -0300

    Tabela de produtos

---
+++ src/components/Tabela/tabela.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import './tabela.css';
+import editar from '../../assets/icons8-editar2.svg';
+import excluir from '../../assets/icons8-lixo4.svg';
+import ModalConfirmar from "../ModalConfirmar";
+import { listarProdutos } from '../../services/apiProdutos';
+import useGlobal from "../../hooks/useGlobal";
+
+function Tabela() {
+    const {
+        setId,
+        condicao, 
+        setCondicao,
+        setModalEdicaoProduto,
+        dadosTodosProdutos, 
+        setDadosTodosProdutos,
+        modalEdicaoProduto,
+        modalCadastroProduto
+      } = useGlobal();
+
+    useEffect(() => {
+        listarProdutos(setDadosTodosProdutos);
+    }, []);
+
+    useEffect(() => {
+        listarProdutos(setDadosTodosProdutos);
+    }, [condicao, modalCadastroProduto, modalEdicaoProduto]);
+
+    return (
+        <div className="table">
+            <div className="table-head">
+                <div className="column-title">
+                    Nome
+                </div>
+                <div className="column-title">
+                    Descrição
+                </div>
+                <div className="column-title">
+                    Valor
+                </div>
+                <div className="column-title">
+                    Quantidade
+                </div>
+            </div>
+
+            <div className="table-body">
+
+                {dadosTodosProdutos && dadosTodosProdutos.map(info => {
+                    return (
+                        <div key={info.id} className="table-line">
+                            <div className="line-items">{info.nome}</div>
+                            <div className="line-items">{info.descricao}</div>
+                            <div className="line-items">{(info.valor).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</div>
+                            <div className="line-items">{info.quantidade}</div>
+                            <div className="line-items">
+                                <img id={info.id}
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={(e) => {
+                                        setModalEdicaoProduto(true);
+                                        setId(Number(e.target.id));
+                                    }}
+                                    className="edit-icon"
+                                    src={editar}
+                                    alt="edit-icon"
+                                />
+
+                                <img id={info.id}
+                                    style={{ cursor: 'pointer' }}
+                                    className="delete-icon"
+                                    onClick={(e) => {
+                                        setId(Number(e.target.id));
+                                        setCondicao(true);
+                                    }}
+                                    src={excluir} alt="delete-icon"
+                                />
+                            </div>
+
+                            <div className="div-confirm-delete">
+                                <ModalConfirmar
+                                    idModal={info.id}
+                                />
+                            </div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+        </div>
+
+    )
+}
+
+export default Tabela;
