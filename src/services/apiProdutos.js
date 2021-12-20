c7198a07d19ae375c0d286f18557a5d43e55cdb6
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-12-20 13:29:57 -0300

    criação da apiProdutos para os fetch

---
+++ src/services/apiProdutos.js
@@ -0,0 +1,110 @@
+const url_Base = `https://mini-delivery.herokuapp.com/produtos`;
+
+async function cadastrarProduto(
+  dados, 
+  setDadosTodosProdutos, 
+  setModalCadastroProduto,
+  setModalEdicaoProduto,
+  limparInputs
+) {
+
+  try {
+    const pedido = await fetch(`${url_Base}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
+      body: JSON.stringify(dados)
+    });
+
+    const { status } = pedido;
+    if (status === 200) {
+      setModalCadastroProduto(false);
+      setModalEdicaoProduto(false);
+      limparInputs()
+    }
+      listarProdutos(setDadosTodosProdutos);
+
+  } catch ({ message }) {
+    console.log(message)
+  }
+
+}
+
+async function atualizarProduto(
+  id, 
+  dados, 
+  setDadosTodosProdutos, 
+  setModalEdicaoProduto, 
+  setModalCadastroProduto,
+  limparInputs
+) {
+
+  try {
+    const pedido = await fetch(`${url_Base}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+    });
+
+    const { status } = pedido;
+    if (status === 200) {
+      setModalCadastroProduto(false);
+      setModalEdicaoProduto(false);
+      limparInputs()
+    }
+    listarProdutos(setDadosTodosProdutos);
+
+  } catch ({ message }) {
+    console.log(message)
+  }
+
+}
+
+async function listarProdutos(setDadosTodosProdutos) {
+
+  try {
+    const response = await fetch(`${url_Base}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const dados = await response.json()
+    setDadosTodosProdutos(dados)
+  } catch ({ message }) {
+    console.log(message)
+  }
+
+}
+
+async function deletarProduto(id, setCondicao, setDadosTodosProdutos) {
+
+  try {
+    const pedido = await fetch(`${url_Base}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const resposta = await pedido.json();
+    setCondicao(false);
+    listarProdutos(setDadosTodosProdutos);
+
+  } catch ({ message }) {
+    console.log(message)
+  }
+
+}
+
+export {
+  cadastrarProduto,
+  atualizarProduto,
+  listarProdutos,
+  deletarProduto,
+}
