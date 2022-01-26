b862636dbd407250daf8437d05b1656bdb459438
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-26 12:59:32 -0300

    Criação do componente Variants

---
+++ src/components/Variants/index.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+
+const criarSkeleton = () => {
+  return (
+    <Skeleton animation="wave" variant="text" width={813} height={80} />
+  )
+}
+
+export default function Variants() {
+  return (
+    <Stack  marginTop={-7} spacing={-3.5}>
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+      {criarSkeleton()}
+    </Stack>
+  );
+}
