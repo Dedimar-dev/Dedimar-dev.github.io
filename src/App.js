3ea1786ea6ca14725d7f9dd55a78a5820c9b161d
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-29 12:27:44 -0300

    Importando o componente Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Tabela from './components/Tabela/tabela';
-import Divfood from './components/divfood';
+import Tabela from './components/Tabela';
+import Divfood from './components/DivFood';
 import ModalProdutoCadastroEdicao from './components/ModalProdutoCadastroEdicao';
+import Header from './components/Header';
 import { GlobalProvider } from './context/GlobalContext';
 
 function App() {
   return (
     <GlobalProvider>
       <div className="App">
+        <Header/>
         <Divfood />
         <Tabela />
         <ModalProdutoCadastroEdicao />
