aa6ac52d81ccd0e842e26c74e6c7dbffb8a15bc4
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-02 15:57:24 -0300

    manutenção

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,14 +3,21 @@ import Tabela from '../../components/Tabela';
 import DivFood from '../../components/divfood';
 import ModalProdutoCadastroEdicao from '../../components/ModalProdutoCadastroEdicao';
 import Header from '../../components/Header';
+import Manutencao from '../../components/Manutencao';
+
 
 function Home() {
+  let condicao = false
   return (
     <div className="Home">
-        <Header/>
-        <DivFood />
-        <Tabela />
-        <ModalProdutoCadastroEdicao />
+      {!condicao && <Manutencao/>}
+       { condicao && <div>
+          <Header/>
+          <DivFood />
+          <Tabela />
+          <ModalProdutoCadastroEdicao />
+        </div>}
+        
     </div>
   );
 }
---
+++ src/components/Manutencao/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+
+function Manutencao() {
+
+ return (
+  <div className="manutencao">
+    
+  </div>
+ )
+}
+
+export default Manutencao;
