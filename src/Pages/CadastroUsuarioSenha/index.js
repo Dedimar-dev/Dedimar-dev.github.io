43af330435199347fedb2b1f3557a08261baa756
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-04 17:57:28 -0300

    Implementação do cadastro da senha do usuário

--- src/Pages/CadastroUsuarioSenha/index.js
+++ src/Pages/CadastroUsuarioSenha/index.js
@@ -1,6 +1,52 @@
-import './style.css'
+import { useState } from 'react';
+import useGlobal from '../../hooks/useGlobal';
+import './style.css';
+import { validaSenhaInputUsuario } from '../../utils/validaSenhaInputUsuario';
+import { cadastrarUsuario } from '../../services/apiUsuario';
+import trataTexto from '../../utils/trataTexto'
+import { useNavigate } from 'react-router-dom';
+import { procuraPalavra } from '../../utils/procuraPalavra';
 
 function CadastroUsuarioSenha() {
+  const navigate = useNavigate();
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+
+  const {
+    dadosUsuario,
+    setDadosUsuario,
+    erroDadosUsuario,
+    setErroDadosUsuario,
+    mensagemDeErroSenha,
+    setMensagemDeErroSenha,
+    mensagemApiCadastroUsuario,
+    setMensagemApiCadastroUsuario
+  } = useGlobal();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const ParametrosParaValidacao = {
+      erroDadosUsuario,
+      setErroDadosUsuario,
+      dadosUsuario,
+      confirmarSenha,
+      setMensagemDeErroSenha
+    }
+    const senhasInvalidas = validaSenhaInputUsuario(ParametrosParaValidacao);
+    if (senhasInvalidas) return
+
+    const ParametrosParaCadatroUsuario = {
+      dadosUsuario,
+      setMensagemDeErroSenha,
+      setMensagemApiCadastroUsuario,
+      setDadosUsuario,
+      setConfirmarSenha,
+      procuraPalavra,
+      navigate
+    }
+    cadastrarUsuario(ParametrosParaCadatroUsuario);
+  }
+
   return (
     <div className="container-cadastro_senha">
       <h1 className="container-cadastro_senha-titulo">
@@ -8,32 +54,83 @@ function CadastroUsuarioSenha() {
       </h1>
 
       <div className="div-form-cadastro_senha">
-        <form className="form-cadastro_senha">
+        <form
+          onSubmit={handleSubmit}
+          className="form-cadastro_senha"
+        >
           <div className="div-form-titulo">
             <h1>Cadastre sua loja</h1>
             <h3>Gerencie sua loja de forma fácil e rápida</h3>
           </div>
-        
+
           <div className="div-form-email">
             <label htmlFor="Senha">Senha*</label>
-            <input 
-              id="Senha" 
-              type="text" 
+            <input
+              className={`
+                form-cadastro_senha_input 
+                ${erroDadosUsuario.erroSenha ||
+                  mensagemDeErroSenha ? 'erroInput' : ''}`
+              }
+              id="Senha"
+              type="text"
+              value={dadosUsuario.senha}
+              placeholder="Digite sua senha"
+              onChange={(event) => {
+                setDadosUsuario({ ...dadosUsuario, senha: event.target.value });
+                setErroDadosUsuario({ ...erroDadosUsuario, erroSenha: false })
+                setMensagemDeErroSenha('');
+              }}
             />
+            <p
+              className={erroDadosUsuario.erroSenha ? 'mensagemErroInput' : 'hidden'}
+            >
+              Este campo deve ser preenchido.
+            </p>
           </div>
 
           <div className="div-form-email">
             <label htmlFor="confirme_senha">Confirme sua Senha*</label>
-            <input 
-              id="confirme_senha" 
-              type="text" 
+            <input
+              className={`
+                form-cadastro_senha_input 
+                ${erroDadosUsuario.erroConfirmarSenha ||
+                  mensagemDeErroSenha ? 'erroInput' : ''}`
+              }
+              id="confirme_senha"
+              type="text"
+              value={confirmarSenha}
+              placeholder="Confirme sua senha"
+              onChange={(event) => {
+                setConfirmarSenha(event.target.value);
+                setErroDadosUsuario({ ...erroDadosUsuario, erroConfirmarSenha: false })
+                setMensagemDeErroSenha('');
+              }}
             />
+            <p
+              className={erroDadosUsuario.erroConfirmarSenha ? 'mensagemErroInput' : 'hidden'}
+            >
+              Este campo deve ser preenchido.
+            </p>
+            <p
+              className={mensagemDeErroSenha ? 'mensagemErroInput' : 'hidden'}
+            >
+              {trataTexto(mensagemDeErroSenha)}
+            </p>
           </div>
 
-          <button>Continuar</button>          
+          <button>
+            Confirmar cadastro
+          </button>
+
+          <div className="navigate-login">
+            <p>Já possui uma conta? Faça seu</p>
+            <span onClick={() => navigate('/login')}>
+              Login
+            </span>
+          </div>
         </form>
       </div>
-      
+
     </div>
   );
 }
