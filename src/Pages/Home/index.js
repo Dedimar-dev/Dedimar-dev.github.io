1e7f5ba4c6cf643790782b8e231f26b7ea66c526
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-04 17:53:37 -0300

    Mudança na pagina home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,21 +3,16 @@ import Tabela from '../../components/Tabela';
 import DivFood from '../../components/divfood';
 import ModalProdutoCadastroEdicao from '../../components/ModalProdutoCadastroEdicao';
 import Header from '../../components/Header';
-import Manutencao from '../../components/Manutencao';
-
+// import Manutencao from '../../components/Manutencao';
 
 function Home() {
-  let condicao = false
+
   return (
     <div className="Home">
-      {!condicao && <Manutencao/>}
-       { condicao && <div>
-          <Header/>
-          <DivFood />
-          <Tabela />
-          <ModalProdutoCadastroEdicao />
-        </div>}
-        
+      <Header/>
+      <DivFood />
+      <Tabela />
+      <ModalProdutoCadastroEdicao />        
     </div>
   );
 }
