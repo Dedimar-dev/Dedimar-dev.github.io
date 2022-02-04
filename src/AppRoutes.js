ae4104995b23d8dda39c667e8b1a17f24925fa31
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-04 17:49:43 -0300

    implementação das rotas protegidas

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,16 +2,39 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import CadastroUsuario from './Pages/CadastroUsuario';
 import CadastroUsuarioSenha from './Pages/CadastroUsuarioSenha';
+import CadastroUsuarioSucesso from './Pages/CadastroUsuarioSucesso';
 import { GlobalProvider } from './context/GlobalContext';
+import {validar_token} from './services/apiUsuario';
 
 import { 
   BrowserRouter as Roteador,
   Route,
-  Routes
+  Routes,
+  useNavigate
 } from 'react-router-dom';
+import useGlobal from './hooks/useGlobal';
+import { useEffect } from 'react';
 
 function RotasProtegidas({children}) {
+  const navigate = useNavigate()
 
+  const {
+    token,
+    expirationToken,
+    setExpirationToken,
+    setAtualUsuario
+  } = useGlobal();
+
+  useEffect(() => {
+    validar_token(token, setExpirationToken, setAtualUsuario);
+  },[token, setExpirationToken, setAtualUsuario])
+
+  if (expirationToken === 'jwt malformed' || 
+      expirationToken === 'Não autorizado' || 
+      expirationToken === 'invalid signature' ||
+      !token) {
+    navigate('/login')
+  }
   return children
 }
 
@@ -22,6 +45,7 @@ function AppRoutes() {
           <Routes>
             <Route path="/cadastrodados" exact element={<CadastroUsuario/>}></Route>
             <Route path="/cadastrosenha" exact element={<CadastroUsuarioSenha/>}></Route>
+            <Route path="/cadastrosucesso" exact element={<CadastroUsuarioSucesso/>}></Route>
             <Route path="/login" exact element={<Login/>}/>
             <Route exact path="/" element={
               <RotasProtegidas>
