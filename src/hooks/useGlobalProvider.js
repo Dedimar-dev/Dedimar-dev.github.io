1883594d10b159109b8dbbd7229624106b5fb2ae
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-04 17:56:12 -0300

    Criação de novas variáveis

--- src/hooks/useGlobalProvider.js
+++ src/hooks/useGlobalProvider.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import { useLocalStorage } from 'react-use';
 
 export default function useGlobalProvider() {
   const [modalCadastroProduto, setModalCadastroProduto] = useState(false);
   const [modalEdicaoProduto, setModalEdicaoProduto] = useState(false);
   const [condicao, setCondicao] = useState(false);
   const [id, setId] = useState(0);
-  const [dadosTodosProdutos, setDadosTodosProdutos] = useState([])
+  const [dadosTodosProdutos, setDadosTodosProdutos] = useState([]);
+  const [mensagemDeErroSenha, setMensagemDeErroSenha] = useState('');
+  const [mensagemApiCadastroUsuario, setMensagemApiCadastroUsuario] = useState('');
+  const [token, setToken] = useLocalStorage('token', '');
+  const [expirationToken, setExpirationToken] = useState('');
+  const [atualUsuario, setAtualUsuario] = useLocalStorage('atual_usuario', '');
   const [dadosProduto, setDadosProduto] = useState({
     nome: '',
     descricao: '',
@@ -14,32 +20,87 @@ export default function useGlobalProvider() {
   });
 
   const [erroDadosProduto, setErroDadosProduto] = useState({
-      erroDescricao: false,
-      erroQuantidade: false,
-      erroValor: false,
-      erroNome: false
+    erroDescricao: false,
+    erroQuantidade: false,
+    erroValor: false,
+    erroNome: false
+  });
+
+  const [dadosUsuario, setDadosUsuario] = useState({
+    nome: '',
+    email: '',
+    senha: '',
+    nome_loja: ''
+  });
+
+  const [erroDadosUsuario, setErroDadosUsuario] = useState({
+    erroNome: false,
+    erroEmail: false,
+    erroSenha: false,
+    erroNovaSenha: false,
+    erroConfirmarSenha: false,
+    erroNome_loja: false,
+    nomeInvalido: false,
+  });
+
+  const [erroDadosUsuarioLogin, setErroDadosUsuarioLogin] = useState({
+    erroEmail: false,
+    erroSenha: false,
+  });
+
+  const [mensagemErroLogin, setMensagemErroLogin] = useState({
+    emailInvalido: '',
+    senhaInvalida: '',
+    notEmailSenha:''
   });
 
   return {
-    modalCadastroProduto, 
+    modalCadastroProduto,
     setModalCadastroProduto,
 
-    modalEdicaoProduto, 
+    modalEdicaoProduto,
     setModalEdicaoProduto,
 
-    condicao, 
+    condicao,
     setCondicao,
 
-    id, 
+    id,
     setId,
-    
-    dadosTodosProdutos, 
+
+    dadosTodosProdutos,
     setDadosTodosProdutos,
 
-    dadosProduto, 
+    dadosProduto,
     setDadosProduto,
 
-    erroDadosProduto, 
-    setErroDadosProduto
+    erroDadosProduto,
+    setErroDadosProduto,
+
+    dadosUsuario,
+    setDadosUsuario,
+
+    erroDadosUsuario,
+    setErroDadosUsuario,
+
+    mensagemDeErroSenha,
+    setMensagemDeErroSenha,
+
+    mensagemApiCadastroUsuario,
+    setMensagemApiCadastroUsuario,
+
+    token,
+    setToken,
+
+    expirationToken, 
+    setExpirationToken,
+
+    erroDadosUsuarioLogin, 
+    setErroDadosUsuarioLogin,
+
+    mensagemErroLogin, 
+    setMensagemErroLogin,
+
+    atualUsuario, 
+    setAtualUsuario
   }
 }
