21418c57aecc00dd4a97c73b1860178b982b39ce
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-02-07 16:03:27 -0300

    Mais proteção nas rotas protegidas

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,19 +4,21 @@ import CadastroUsuario from './Pages/CadastroUsuario';
 import CadastroUsuarioSenha from './Pages/CadastroUsuarioSenha';
 import CadastroUsuarioSucesso from './Pages/CadastroUsuarioSucesso';
 import { GlobalProvider } from './context/GlobalContext';
-import {validar_token} from './services/apiUsuario';
+import { validar_token } from './services/apiUsuario';
 
-import { 
+import {
   BrowserRouter as Roteador,
   Route,
   Routes,
-  useNavigate
+  Navigate,
+  useLocation
 } from 'react-router-dom';
 import useGlobal from './hooks/useGlobal';
 import { useEffect } from 'react';
 
-function RotasProtegidas({children}) {
-  const navigate = useNavigate()
+
+function RotasProtegidas({ children }) {
+  const { pathname} = useLocation();
 
   const {
     token,
@@ -27,34 +29,38 @@ function RotasProtegidas({children}) {
 
   useEffect(() => {
     validar_token(token, setExpirationToken, setAtualUsuario);
-  },[token, setExpirationToken, setAtualUsuario])
+  }, [token, setExpirationToken, setAtualUsuario])
 
-  if (expirationToken === 'jwt malformed' || 
-      expirationToken === 'Não autorizado' || 
-      expirationToken === 'invalid signature' ||
-      expirationToken === 'jwt expired' ||
-      !token) {
-    navigate('/login')
-  }
-  return children
+  if (expirationToken === 'jwt malformed' ||
+    expirationToken === 'Não autorizado' ||
+    expirationToken === 'invalid signature' ||
+    expirationToken === 'jwt expired' ||
+    !token) {
+
+    if (pathname !== 'home') {
+      return (<Navigate to="/" />)
+    }
+  };
+
+  return (children)
 }
 
 function AppRoutes() {
   return (
     <GlobalProvider>
-        <Roteador>
-          <Routes>
-            <Route path="/cadastrodados" exact element={<CadastroUsuario/>}></Route>
-            <Route path="/cadastrosenha" exact element={<CadastroUsuarioSenha/>}></Route>
-            <Route path="/cadastrosucesso" exact element={<CadastroUsuarioSucesso/>}></Route>
-            <Route path="/login" exact element={<Login/>}/>
-            <Route exact path="/" element={
-              <RotasProtegidas>
-                <Home/>
-              </RotasProtegidas>
-            }/>
-          </Routes>
-        </Roteador>
+      <Roteador>
+        <Routes>
+          <Route path="/cadastrodados" exact element={<CadastroUsuario/>}></Route>
+          <Route path="/cadastrosenha" exact element={<CadastroUsuarioSenha/>}></Route>
+          <Route path="/cadastrosucesso" exact element={<CadastroUsuarioSucesso/>}></Route>
+          <Route path="/" exact element={<Login/>}/>
+          <Route exact path="/home" element={
+            <RotasProtegidas>
+              <Home/>
+            </RotasProtegidas>
+          } />
+        </Routes>
+      </Roteador>
     </GlobalProvider>
 
   );
